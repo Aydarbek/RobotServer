53cd57fd7c6cb756ee5d7e5e3470f458bf8ab732
Aydar Akhmetshin <a88236@gmail.com>
2019-02-03 23:33:39 +0300

    delete tracked folders

--- temp/data/done/190102.195500.1.cs/Program.cs
+++
@@ -1,17 +0,0 @@
-try {
-using System;
-public class Program
-{	
-	public static void Main()
-	{
-
-				
-		int a = int.Parse(Console.ReadLine());		
-		int b = int.Parse(Console.ReadLine());		
-		int c = a + b;		
-		Console.WriteLine(c);	
-		
-
-	}
-}
-} catch {}
